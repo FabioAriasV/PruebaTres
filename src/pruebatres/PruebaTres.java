a609e6e0058e37d1f122a3b54419788aa80f089a
Andres <Andres@LAPTOP-85020IHJ>
2019-10-08 19:04:34 -0500

    Practica

---
+++ src/pruebatres/PruebaTres.java
@@ -0,0 +1,26 @@
+package pruebatres;
+
+public class PruebaTres {
+
+    
+        int numeroUno;
+        int numeroDos;
+
+    public int getNumeroUno() {
+        return numeroUno;
+    }
+
+    public void setNumeroUno(int numeroUno) {
+        this.numeroUno = numeroUno;
+    }
+
+    public int getNumeroDos() {
+        return numeroDos;
+    }
+
+    public void setNumeroDos(int numeroDos) {
+        this.numeroDos = numeroDos;
+    }
+    }
+
+
